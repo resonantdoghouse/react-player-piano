73e19da189f05631f051093eaa4e378c0ab3bce0
Jim <jim@advancebass.com>
2026-01-04 11:58:20 -0800

    Migrate project to Vite and fix Sass deprecation

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+  plugins: [react(), svgr()],
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    open: true,
+    port: 3000,
+  },
+});
